8983518c85ef481799150c80f9bc1706b4b64d2c
Akhil Nair <yup.im.aki@gmail.com>
2020-04-25 22:41:48 +0100

    Add tmux config

--- dotfiles/ubuntu/install.sh
+++ dotfiles/ubuntu/install.sh
@@ -1,18 +1,28 @@
 #!/bin/bash -eu
 
+repo=$(git rev-parse --show-toplevel)
+
 mkdir -p "${HOME}/bkp-dotfiles"
 
 # Install .bash* files, replace history
 mv ${HOME}/.bash* ${HOME}/bkp-dotfiles 2>/dev/null || true
-cp ${HOME}/repos/home-setup/dotfiles/ubuntu/.bash* ${HOME}
+cp ${repo}/dotfiles/ubuntu/.bash* ${HOME}
 mv ${HOME}/bkp-dotfiles/.bash_history ${HOME} || true
 
 # Install .gitconfig
 mv ${HOME}/.gitconfig ${HOME}/bkp-dotfiles 2>/dev/null || true
-cp ${HOME}/repos/home-setup/dotfiles/ubuntu/.gitconfig ${HOME}
+cp ${repo}/dotfiles/ubuntu/.gitconfig ${HOME}
 
 # Install kakrc
-ln -s $(pwd)/kak/ $XDG_CONFIG_HOME/kak
+ln -s ${repo}/ubuntu/kak/ ~/.config/kak 2>/dev/null || true
+
+echo "Installing tmux"
+# Install tmux plugin manager
+TMUX_HOME="${HOME}/.tmux/"
+# git clone https://github.com/tmux-plugins/tpm ${TMUX_HOME}/plugins/tpm
+cp ${repo}/dotfiles/ubuntu/.tmux.conf ${HOME}
+${TMUX_HOME}/plugins/tpm/bin/install_plugins
+${TMUX_HOME}/plugins/tpm/bin/update_plugins all
 
 source "${HOME}/.bashrc"
 
